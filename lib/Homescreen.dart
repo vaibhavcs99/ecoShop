e792c71893bf5fd5882e60021c42ac36a29fd608
Raj Burundkar <rajburundkar@gmail.com>
2020-11-07 22:13:17 +0530

    added splash screen

---
+++ lib/Homescreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
