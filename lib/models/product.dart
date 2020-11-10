e3dba77904b2da49fc121751af39e28b1aa8f254
Raj Burundkar <rajburundkar@gmail.com>
2020-11-10 09:31:14 +0530

    fixed signup, added product class

---
+++ lib/models/product.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool favorite;
+
+  Product(
+      {@required this.id,
+      @required this.title,
+      @required this.description,
+      @required this.price,
+      @required this.imageUrl,
+      @required this.favorite});
+}
--- lib/screens/signuppage.dart
+++ lib/screens/signuppage.dart
@@ -22,201 +22,207 @@ class _SignupPageState extends State<SignupPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.all(20),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Welcome!",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontFamily: 'sfpro',
-                  fontWeight: FontWeight.w900,
-                ),
-              ),
-              Text(
-                "Please enter the details",
-                style: TextStyle(
-                  color: Colors.black38,
-                  fontFamily: 'poppins',
-                  fontSize: 18,
-                  fontWeight: FontWeight.w900,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Your Name",
-                    labelStyle: TextStyle(
-                        color: Colors.grey, fontWeight: FontWeight.w100),
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40),
-                      borderSide: const BorderSide(width: 0.1),
+        padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Welcome!",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontFamily: 'sfpro',
+                      fontWeight: FontWeight.w900,
                     ),
-                    focusedBorder: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(40),
-                      borderSide: BorderSide(width: 0.1),
-                    )),
-                validator: (val) {
-                  if (val.length == 0) {
-                    return "Name cannot be empty";
-                  } else {
-                    return null;
-                  }
-                },
-                style: new TextStyle(
-                  fontFamily: 'poppins',
-                ),
-              ),
-                            SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Phone",
-                    labelStyle: TextStyle(
-                        color: Colors.grey, fontWeight: FontWeight.w100),
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40),
-                      borderSide: const BorderSide(width: 0.1),
-                    ),
-                    focusedBorder: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(40),
-                      borderSide: BorderSide(width: 0.1),
-                    )),
-                validator: (val) {
-                  if (val.length == 0) {
-                    return "Phone cannot be empty";
-                  } else {
-                    return null;
-                  }
-                },
-                keyboardType: TextInputType.number,
-                style: new TextStyle(
-                  fontFamily: 'poppins',
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Email",
-                    labelStyle: TextStyle(
-                        color: Colors.grey, fontWeight: FontWeight.w100),
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40),
-                      borderSide: const BorderSide(width: 0.1),
-                    ),
-                    focusedBorder: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(40),
-                      borderSide: BorderSide(width: 0.1),
-                    )),
-                validator: (val) {
-                  if (val.length == 0) {
-                    return "Email cannot be empty";
-                  } else {
-                    return null;
-                  }
-                },
-                keyboardType: TextInputType.emailAddress,
-                style: new TextStyle(
-                  fontFamily: 'poppins',
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Create Password",
-                    labelStyle: TextStyle(
-                        color: Colors.grey, fontWeight: FontWeight.w100),
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40),
-                      borderSide: const BorderSide(width: 0.1),
-                    ),
-                    focusedBorder: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(40),
-                      borderSide: BorderSide(width: 0.1),
-                    )),
-                validator: (val) {
-                  if (val.length == 0) {
-                    return "Password cannot be empty";
-                  } else {
-                    return null;
-                  }
-                },
-                obscureText: true,
-                style: new TextStyle(
-                  fontFamily: 'poppins',
-                ),
-              ),
-                            SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Confirm Password",
-                    labelStyle: TextStyle(
-                        color: Colors.grey, fontWeight: FontWeight.w100),
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40),
-                      borderSide: const BorderSide(width: 0.1),
-                    ),
-                    focusedBorder: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(40),
-                      borderSide: BorderSide(width: 0.1),
-                    )),
-                validator: (val) {
-                  if (val.length == 0) {
-                    return "Password cannot be empty";
-                  } else {
-                    return null;
-                  }
-                },
-                obscureText: true,
-                style: new TextStyle(
-                  fontFamily: 'poppins',
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => BottomTabs()));
-                },
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(40)),
-                    gradient: gradientButtons,
                   ),
-                  child: Center(
-                    child: Text(
-                      "Signup",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'sfpro',
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+                  Text(
+                    "Please enter the details",
+                    style: TextStyle(
+                      color: Colors.black38,
+                      fontFamily: 'poppins',
+                      fontSize: 18,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Your Name",
+                        labelStyle: TextStyle(
+                            color: Colors.grey, fontWeight: FontWeight.w100),
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                          borderSide: const BorderSide(width: 0.1),
+                        ),
+                        focusedBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(40),
+                          borderSide: BorderSide(width: 0.1),
+                        )),
+                    validator: (val) {
+                      if (val.length == 0) {
+                        return "Name cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                    style: new TextStyle(
+                      fontFamily: 'poppins',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Phone",
+                        labelStyle: TextStyle(
+                            color: Colors.grey, fontWeight: FontWeight.w100),
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                          borderSide: const BorderSide(width: 0.1),
+                        ),
+                        focusedBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(40),
+                          borderSide: BorderSide(width: 0.1),
+                        )),
+                    validator: (val) {
+                      if (val.length == 0) {
+                        return "Phone cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                    keyboardType: TextInputType.number,
+                    style: new TextStyle(
+                      fontFamily: 'poppins',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                            color: Colors.grey, fontWeight: FontWeight.w100),
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                          borderSide: const BorderSide(width: 0.1),
+                        ),
+                        focusedBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(40),
+                          borderSide: BorderSide(width: 0.1),
+                        )),
+                    validator: (val) {
+                      if (val.length == 0) {
+                        return "Email cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    style: new TextStyle(
+                      fontFamily: 'poppins',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Create Password",
+                        labelStyle: TextStyle(
+                            color: Colors.grey, fontWeight: FontWeight.w100),
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                          borderSide: const BorderSide(width: 0.1),
+                        ),
+                        focusedBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(40),
+                          borderSide: BorderSide(width: 0.1),
+                        )),
+                    validator: (val) {
+                      if (val.length == 0) {
+                        return "Password cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                    obscureText: true,
+                    style: new TextStyle(
+                      fontFamily: 'poppins',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Confirm Password",
+                        labelStyle: TextStyle(
+                            color: Colors.grey, fontWeight: FontWeight.w100),
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                          borderSide: const BorderSide(width: 0.1),
+                        ),
+                        focusedBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(40),
+                          borderSide: BorderSide(width: 0.1),
+                        )),
+                    validator: (val) {
+                      if (val.length == 0) {
+                        return "Password cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                    obscureText: true,
+                    style: new TextStyle(
+                      fontFamily: 'poppins',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => BottomTabs()));
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(40)),
+                        gradient: gradientButtons,
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Signup",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'sfpro',
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-            ]),
+                ]),
+          ),
+        ),
       ),
     );
   }
